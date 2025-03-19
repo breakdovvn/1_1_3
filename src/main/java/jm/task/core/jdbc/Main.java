31aad481252bf9f858ec2c1e5374fef577bed7df
Kamil <kamil.arslanoffx@gmail.com>
2025-03-19 22:18:57 +0500

    первый коммит

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -7,18 +7,19 @@ import jm.task.core.jdbc.util.Util;
 
 public class Main {
     public static void main(String[] args) {
-//        UserDao userDao = new UserDaoHibernateImpl();
-//
-//        userDao.createUsersTable();
-//
-//        userDao.saveUser("John", "Doe", (byte) 30);
-//        userDao.saveUser("Alice", "Smith", (byte) 25);
-//
-//        System.out.println(userDao.getAllUsers());
-//
-//        userDao.cleanUsersTable();
-//        userDao.dropUsersTable();
-//
-//        Util.shutdown();
+        Util.getConnection();
+        UserDao userDao = new UserDaoJDBCImpl();
+
+        userDao.createUsersTable();
+
+        userDao.saveUser("Name1", "LastName1", (byte) 20);
+        userDao.saveUser("Name2", "LastName2", (byte) 25);
+        userDao.saveUser("Name3", "LastName3", (byte) 31);
+        userDao.saveUser("Name4", "LastName4", (byte) 38);
+
+        userDao.removeUserById(1);
+        userDao.getAllUsers();
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
     }
 }
